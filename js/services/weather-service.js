69176fb97252a1b36e387975076c77048ea42090
Nadav Julius <nadavjulius@gmail.com>
2020-02-19 14:49:08 +0200

    'adding axios'

--- js/services/weather-service.js
+++ js/services/weather-service.js
@@ -1,8 +1,24 @@
 'use strict'
 
+export var weatherService = {
+
+}
+
+const WEATHER_KEY = '0842198d900c6bb69d303eab2504a6fa'
+
+
+function getCurrentWeather
 
 
 
+
+
+
+
+
+
+
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
 
 
 
